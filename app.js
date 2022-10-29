61ea0702b909bf24b982007dc400b8ea03b1cb74
khiati-walid <khiatikhaledwalid@gmail.com>
2022-10-29 15:30:29 +0100

    feat: Task model

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routers/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const NotFound = require("./middleware/not-found");
+
+// Middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/tasks", tasks);
+app.use(NotFound);
+
+const port = 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server listening in ${port} ...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
